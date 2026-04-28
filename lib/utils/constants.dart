556fdd15ab07d53355da75e29ee91c0a706ab2a9
GraviQuate <hindaaaaaaaaaaaaa@gmail.com>
2026-04-28 22:13:21 +0800

    initial draft of error snackbar

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -40,6 +40,7 @@ abstract class AppDesign {
   static const Color appPaleCyan = Color(0xffe2fff8);
   static const Color appOffblack = Color(0xff302e34);
   static const Color appLightGray = Color(0xfff1f1f1);
+  static const Color errorRed = Color(0xfff44336);
   // static const Color white = Color(0xffffffff); just use Colors.white
 
   // Gradient Colors
---
+++ lib/widgets/error_snackbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:tracer/utils/constants.dart';
+
+class ErrorSnackbar extends SnackBar {
+  final String errorMsg;
+
+  ErrorSnackbar({
+    super.key,
+    required this.errorMsg,
+  }) : super(
+          // Snackbar properties
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+
+          // Animation of the snackbar
+          content: TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0.0, end: 1.0),
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.elasticOut,
+
+            builder: (context, value, child) {
+              return Transform.scale(
+                scale: value,
+                child: child,
+              );
+            },
+
+            // Container with red border
+            child: Container(
+              decoration: BoxDecoration(
+                border: BoxBorder.all(
+                  color: AppDesign.errorRed,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+
+              // Icon and message
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.error_outline_rounded,
+                      color: AppDesign.errorRed,
+                      size: 36.0,
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      errorMsg,
+                      style: const TextStyle(
+                        color: AppDesign.appOffblack,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "AROneSans"
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+}
